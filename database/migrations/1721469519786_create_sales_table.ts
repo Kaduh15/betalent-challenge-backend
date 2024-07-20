96d578e878357227907fb3a0503d509f7ecbb4c7
Kadu <kadu.silva2014@gmail.com>
2024-07-20 07:01:05 -0300

    Feat: create sales table

---
+++ database/migrations/1721469519786_create_sales_table.ts
@@ -0,0 +1,28 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'sales'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table
+        .integer('client_id')
+        .unsigned()
+        .references('id')
+        .inTable('clients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+      table.integer('product_id').unsigned().references('id').inTable('products')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
