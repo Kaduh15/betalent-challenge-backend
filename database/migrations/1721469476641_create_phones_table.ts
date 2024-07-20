dbb873d7215d3b275f23d4f9b8bbadbcb8ebdd28
Kadu <kadu.silva2014@gmail.com>
2024-07-20 07:00:41 -0300

    Feat: create phone table

---
+++ database/migrations/1721469476641_create_phones_table.ts
@@ -0,0 +1,28 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'phones'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+
+      table
+        .integer('client_id')
+        .unsigned()
+        .references('id')
+        .inTable('clients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+      table.string('number').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
